f5126cab47f13cd97560641e8367e3425bc66b7a
David Vallejo <thyngster@gmail.com>
2025-07-14 13:48:37 +0200

    New Version

--- entrypoints/popup/App.vue
+++ entrypoints/popup/App.vue
@@ -71,7 +71,7 @@
           </div>
           
           <div class="text-xs text-gray-500">
-            Version 3.0.0-beta2
+            Version 3.0.0-beta3
           </div>
         </div>
       </div>
